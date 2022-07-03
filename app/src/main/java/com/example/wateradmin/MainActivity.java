ac0918ba2387399f3c9e69bdda0213ffc8dfb12d
CSeoyeon <choiseoyeon0223@gmail.com>
2022-07-03 23:18:36 +0900

    메인-전달받은 데이터 메인에 출력 확인

--- app/src/main/java/com/example/wateradmin/MainActivity.java
+++ app/src/main/java/com/example/wateradmin/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -41,6 +42,5 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
     }
-
 
 }
