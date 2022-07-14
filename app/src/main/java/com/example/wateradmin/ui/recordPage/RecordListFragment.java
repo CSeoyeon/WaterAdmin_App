001e73d8235fb160a20b3f5cc581313a9cdba6c2
Changmin Bae <65561206+baec23@users.noreply.github.com>
2022-07-14 15:39:21 +0900

    recyclerview sample

--- app/src/main/java/com/example/wateradmin/ui/recordPage/RecordListFragment.java
+++ app/src/main/java/com/example/wateradmin/ui/recordPage/RecordListFragment.java
@@ -1,68 +1,51 @@
 package com.example.wateradmin.ui.recordPage;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wateradmin.databinding.FragmentRecordlistBinding;
-import com.example.wateradmin.ui.inputPage.model.UsageRecordDate;
-import com.example.wateradmin.ui.inputPage.model.UsageType;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class RecordListFragment extends ListFragment {
+public class RecordListFragment extends Fragment
+{
 
     private FragmentRecordlistBinding binding;
-
     private RecordListViewModel recordListViewModel;
-    private double waterAmount = 10;
-    private double watertax =10;
+    private RecordRecyclerViewAdapter recordRecyclerViewAdapter;
 
-    private ListView lv_waterData;
-    private List waterUsedRecords = new ArrayList(){};
-    private RecordListAdapter recordListAdapter = new RecordListAdapter();
+    private RecyclerView rv_records;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
 
         recordListViewModel = new ViewModelProvider(this).get(RecordListViewModel.class);
         binding = FragmentRecordlistBinding.inflate(inflater, container, false);
+
+        rv_records = binding.recordListRvRecords;
+        if(recordRecyclerViewAdapter == null)
+            recordRecyclerViewAdapter = new RecordRecyclerViewAdapter(recordListViewModel.getRecordList());
 
         return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
         super.onViewCreated(view, savedInstanceState);
-        setListAdapter(recordListAdapter);
+        rv_records.setLayoutManager(new LinearLayoutManager(requireContext()));
+        rv_records.setAdapter(recordRecyclerViewAdapter);
 
-        //recordListAdapter.addItem(날짜, 수도 사용량, 수도세);
-        //recordListAdapter.addItem(null, waterAmount, watertax);
-
-
-
-       lv_waterData = binding.list;
-       for(Object record: recordListViewModel.getRecordList()){
-           waterUsedRecords.add(record);
-       }
-
-
-        ArrayAdapter<String> recordAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, waterUsedRecords);
-        lv_waterData.setAdapter(recordAdapter);
-
-
+        recordRecyclerViewAdapter.notifyDataSetChanged();
     }
 }
